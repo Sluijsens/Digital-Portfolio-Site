86775a773a87922cc8917bbe54ccb22d7e4dec06
Sluijsens <bdslop@gmail.com>
2013-11-29 18:01:16 +0100

    Added custom post types Added 2 page templates added local config file

---
+++ wp-content/themes/digital-portfolio-lamp/page-about_me.php
@@ -0,0 +1,47 @@
+<?php
+/*
+  Template Name: About Me
+ */
+?>
+
+<?php get_header(); ?>
+
+<div id="content">
+
+    <div id="inner-content" class="wrap clearfix">
+
+        <div id="main" class="eightcol first clearfix" role="main">
+
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <div>
+            </div>
+            
+
+                <?php endwhile;
+            else :
+                ?>
+
+                <article id="post-not-found" class="hentry clearfix">
+                    <header class="article-header">
+                        <h1><?php _e('Oops, Post Not Found!', 'bonestheme'); ?></h1>
+                    </header>
+                    <section class="entry-content">
+                        <p><?php _e('Uh Oh. Something is missing. Try double checking things.', 'bonestheme'); ?></p>
+                    </section>
+                    <footer class="article-footer">
+                        <p><?php _e('This is the error message in the page.php template.', 'bonestheme'); ?></p>
+                    </footer>
+                </article>
+
+<?php endif; ?>
+
+        </div> <!-- end #main -->
+
+<?php get_sidebar(); ?>
+
+    </div> <!-- end #inner-content -->
+
+</div> <!-- end #content -->
+
+<?php get_footer(); ?>
