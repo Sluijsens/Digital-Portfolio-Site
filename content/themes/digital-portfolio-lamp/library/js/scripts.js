fd756460e9a3d59dded1a5ce9c8836c59babf8c5
Sluijsens <bdslop@gmail.com>
2014-02-19 23:38:44 +0100

    Updated wordpress to 3.8.1 and made some progress on the single project page

--- content/themes/digital-portfolio-lamp/library/js/scripts.js
+++ content/themes/digital-portfolio-lamp/library/js/scripts.js
@@ -10,130 +10,185 @@
  */
 
 // IE8 ployfill for GetComputed Style (for Responsive Script below)
-if (!window.getComputedStyle) {
-    window.getComputedStyle = function(el, pseudo) {
+if ( !window.getComputedStyle ) {
+    window.getComputedStyle = function( el, pseudo ) {
         this.el = el;
-        this.getPropertyValue = function(prop) {
+        this.getPropertyValue = function( prop ) {
             var re = /(\-([a-z]){1})/g;
-            if (prop == 'float')
+            if ( 'float' == prop ) {
                 prop = 'styleFloat';
-            if (re.test(prop)) {
-                prop = prop.replace(re, function() {
+            }
+            if ( re.test( prop ) ) {
+                prop = prop.replace( re, function() {
                     return arguments[2].toUpperCase();
-                });
+                } );
             }
             return el.currentStyle[prop] ? el.currentStyle[prop] : null;
-        }
+        };
         return this;
-    }
+    };
 }
 
 // as the page loads, call these scripts
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function( $ ) {
 
     /* Declare functions */
     var project_width = 0;
-    var margin_left = $(".project").css("margin-left");
-    var margin_right = $(".project").css("margin-right");
-    var margin_bottom = $(".project").css("margin-bottom");
-    var margin_top = $(".project").css("margin-top");
+    var margin_left = $( ".project" ).css( "margin-left" );
+    var margin_right = $( ".project" ).css( "margin-right" );
+    var margin_bottom = $( ".project" ).css( "margin-bottom" );
+    var margin_top = $( ".project" ).css( "margin-top" );
     var setSizeProjectsOnOverview = function() {
-        if ($(".project").size() > 0) {
-            var project = $(".project");
-            
+        if ( $( ".project" ).size() > 0 ) {
+            var project = $( ".project" );
+
             project_width = project.width() - 5;
-            
-            project.height(project_width);
 
-            $("img", project).height(project_width);
+            project.height( project_width );
+
+            $( "img", project ).height( project_width );
         }
-    }
-    
-    // Enlarge project details on mouse enter and make them smaller on mous eleave
-    var toggleSizeProjectsOnOverview = function(el, enlarge) {
-        
-        if(enlarge) {
-            var enlarged_width = (project_width * 1.5);
-            var min_margin_top = 15;
-            var new_offset_top = $(el).parent().offset().top;
-            var new_offset_bottom = $(el).parent().offset().top + enlarged_width;
-            console.log(new_offset_bottom + " en " + $("#main").height());
-            el.width(enlarged_width);
-            el.height(enlarged_width);
+    };
 
-            $("img", el).height(enlarged_width);
-            
-            margin_left = parseInt(el.css("margin-left").replace("px", ""));
-            margin_right = parseInt(el.css("margin-right").replace("px", ""));
-            margin_bottom = parseInt(el.css("margin-bottom").replace("px", ""));
-            margin_top = parseInt(el.css("margin-top").replace("px", ""));
-            
-            var new_margin_left = margin_left - ((enlarged_width - project_width) / 2);
+    // Enlarge project details on mouse enter and make them smaller on mous eleave
+    var toggleSizeProjectsOnOverview = function( el, enlarge ) {
+
+        if ( enlarge ) {
+            var enlarged_width = ( project_width * 1.5 );
+            var min_margin_top = 15;
+            var new_offset_top = $( el ).parent().offset().top;
+            var new_offset_bottom = $( el ).parent().offset().top + enlarged_width;
+            console.log( new_offset_bottom + " en " + $( "#main" ).height() );
+            el.width( enlarged_width );
+            el.height( enlarged_width );
+
+            $( "img", el ).height( enlarged_width );
+
+            margin_left = parseInt( el.css( "margin-left" ).replace( "px", "" ) );
+            margin_right = parseInt( el.css( "margin-right" ).replace( "px", "" ) );
+            margin_bottom = parseInt( el.css( "margin-bottom" ).replace( "px", "" ) );
+            margin_top = parseInt( el.css( "margin-top" ).replace( "px", "" ) );
+
+            var new_margin_left = margin_left - ( ( enlarged_width - project_width ) / 2 );
             //alert(margin_left + " en " + new_margin_left + " en " + (enlarged_width - project_width));
-            var new_margin_right = margin_right - ((enlarged_width - project_width) / 2);
-            var new_margin_bottom = margin_bottom - ((enlarged_width - project_width) / 2);
-            var new_margin_top = margin_top - ((enlarged_width - project_width) / 2);
-            
+            var new_margin_right = margin_right - ( ( enlarged_width - project_width ) / 2 );
+            var new_margin_bottom = margin_bottom - ( ( enlarged_width - project_width ) / 2 );
+            var new_margin_top = margin_top - ( ( enlarged_width - project_width ) / 2 );
+
             new_offset_top += new_margin_top;
-            
-            if(new_offset_top < min_margin_top) {
-                new_margin_bottom += (new_margin_top);
+
+            if ( new_offset_top < min_margin_top ) {
+                new_margin_bottom += ( new_margin_top );
                 new_margin_top = margin_top;
-            } else if(new_offset_bottom > $("#main").height()) {
+            } else if ( new_offset_bottom > $( "#main" ).height() ) {
                 new_margin_bottom = 0;
                 new_margin_top = margin_top;
             }
-            
-            el.css({
+
+            el.css( {
                 "margin-left": new_margin_left + "px",
                 "margin-right": new_margin_right + "px",
                 "margin-bottom": new_margin_bottom,
                 "margin-top": new_margin_top
-            });
+            } );
         } else {
-            el.width("");
-            el.height(project_width);
+            el.width( "" );
+            el.height( project_width );
 
-            $("img", el).height(project_width);
-            
-            el.css({
+            $( "img", el ).height( project_width );
+
+            el.css( {
                 "margin-left": "",
                 "margin-right": "",
                 "margin-bottom": "",
                 "margin-top": ""
-            });
+            } );
         }
-    }
-    
+    };
+
 
     var setSizeProjectsOverviewPage = function() {
         var minusHeight = 0;
-        if ($("#wpadminbar").size() > 0) {
-            minusHeight = $("#wpadminbar").height();
+        if ( $( "#wpadminbar" ).size() > 0 ) {
+            minusHeight = $( "#wpadminbar" ).height();
         }
         minusHeight += 0;
 
-        $("#inner-content.projects").height($(window).height() - 57 - minusHeight);
-        $("#container").css({"margin-top": minusHeight + "px"});
+        $( "#inner-content.projects" ).height( $( window ).height() - 57 - minusHeight );
+        $( "#container" ).css( {"margin-top": minusHeight + "px"} );
 
-        if ($("#page-title.projects").size() > 0) {
-            var title_width = $("#page-title.projects h2").width();
+        if ( $( "#page-title.projects" ).size() > 0 ) {
+            var title_width = $( "#page-title.projects h2" ).width();
 
-            $("#page-title.projects h2").css({
+            $( "#page-title.projects h2" ).css( {
                 "bottom": "125px",
                 "right": "-125px"
-            });
+            } );
         }
-    }
+    };
     
-    $.fn.align_vertically = function() {
+    // Plugins
+    // Set the height and with in a scale
+    $.fn.setSizeHighlightedProject = function() {
+
+        // Scale of the highlight
+        var scale_width = 16;
+        var scale_height = 9;
+        var scale = scale_width / scale_height;
         
+        var minus_height = $( '#menu-main-menu' ).height();
+        var margin_top = 0;
+        if ( $( '#wpadminbar' ).size() > 0 ) {
+            minus_height += $( '#wpadminbar' ).height();
+            margin_top = $( '#wpadminbar' ).height();
+        }
+        
+        var viewport_width = $( window ).width();
+        var viewport_height = $( window ).height() -  minus_height;
+        var viewport_scale = viewport_width / viewport_height;
+        
+        var width = viewport_width;
+        var height = ( width / scale );
+        
+        if ( viewport_scale >= scale ) {
+
+            height = viewport_height;
+            width = height * scale;
+            
+        }
+        
+        alert( viewport_width + ' / ' + viewport_height + ' = ' + viewport_scale + ' en ' + scale );
+        
+        $( this ).height( height );
+        $( this ).width( width );
+        $( this ).css({
+            'top': '0px'
+        });
+        $( this ).align_horizontally();
+        
+        var parent_height = viewport_height;
+        var margin_top = ( parent_height / 2 ) - ( height / 2 );
+
+        $( this ).css( 'margin-top', margin_top + 'px' );
+    };
+    // Align center vertically
+    $.fn.align_vertically = function() {
+
         var height = $( this ).height();
         var parent_height = $( this ).parent().height();
-        var margin_top = ( parent_height / 2 ) - ( height / 2);
-        
+        var margin_top = ( parent_height / 2 ) - ( height / 2 );
+
         $( this ).css( 'margin-top', margin_top + 'px' );
-    }
+    };
+    
+    $.fn.align_horizontally = function() {
+
+        var width = $( this ).width();
+        var parent_width = $( this ).parent().width();
+        var margin_left = ( parent_width / 2 ) - ( width / 2 );
+
+        $( this ).css( 'margin-left', margin_left + 'px' );
+    };
 
     /*
      Responsive jQuery is a tricky thing.
@@ -143,45 +198,48 @@ jQuery(document).ready(function($) {
      */
 
     /* getting viewport width */
-    var responsive_viewport = $(window).width();
+    var responsive_viewport = $( window ).width();
 
     /* if is below 481px */
-    if (responsive_viewport < 481) {
+    if ( responsive_viewport < 481 ) {
 
     } /* end smallest screen */
 
     /* if is larger than 481px */
-    if (responsive_viewport > 481) {
+    if ( responsive_viewport > 481 ) {
 
     } /* end larger than 481px */
 
     /* if is above or equal to 768px */
-    if (responsive_viewport >= 768) {
+    if ( responsive_viewport >= 768 ) {
 
         /* load gravatars */
-        $('.comment img[data-gravatar]').each(function() {
-            $(this).attr('src', $(this).attr('data-gravatar'));
-        });
+        $( '.comment img[data-gravatar]' ).each( function() {
+            $( this ).attr( 'src', $( this ).attr( 'data-gravatar' ) );
+        } );
 
         setSizeProjectsOnOverview();
         setSizeProjectsOverviewPage();
-
-        $(window).resize(function() {
+        $( '#highlighted_project' ).setSizeHighlightedProject();
+        $( '#highlighted_project_image #image_container' ).align_vertically();
+        
+        $( window ).resize( function() {
             setSizeProjectsOnOverview();
             setSizeProjectsOverviewPage();
-        });
-        
-        $(".project").on("mouseenter", function() {
-            toggleSizeProjectsOnOverview($(this), true);
-        }).on("mouseleave", function() {
-            toggleSizeProjectsOnOverview($(this), false);
-        });
-        
+            //$( '#highlighted_project' ).setSizeHighlightedProject();
+        } );
+
+        $( ".project" ).on( "mouseenter", function() {
+            toggleSizeProjectsOnOverview( $( this ), true );
+        } ).on( "mouseleave", function() {
+            toggleSizeProjectsOnOverview( $( this ), false );
+        } );
+
         $( '#highlighted_project_image img' ).align_vertically();
     }
 
     /* off the bat large screen actions */
-    if (responsive_viewport > 1030) {
+    if ( responsive_viewport > 1030 ) {
 
     }
 
@@ -189,54 +247,54 @@ jQuery(document).ready(function($) {
     // add all your scripts here
 
 
-}); /* end of as page load scripts */
+} ); /* end of as page load scripts */
 
 
 /*! A fix for the iOS orientationchange zoom bug.
  Script by @scottjehl, rebound by @wilto.
  MIT License.
  */
-(function(w) {
+( function( w ) {
     // This fix addresses an iOS bug, so return early if the UA claims it's something else.
-    if (!(/iPhone|iPad|iPod/.test(navigator.platform) && navigator.userAgent.indexOf("AppleWebKit") > -1)) {
+    if ( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ) {
         return;
     }
     var doc = w.document;
-    if (!doc.querySelector) {
+    if ( !doc.querySelector ) {
         return;
     }
-    var meta = doc.querySelector("meta[name=viewport]"),
-            initialContent = meta && meta.getAttribute("content"),
+    var meta = doc.querySelector( "meta[name=viewport]" ),
+            initialContent = meta && meta.getAttribute( "content" ),
             disabledZoom = initialContent + ",maximum-scale=1",
             enabledZoom = initialContent + ",maximum-scale=10",
             enabled = true,
             x, y, z, aig;
-    if (!meta) {
+    if ( !meta ) {
         return;
     }
     function restoreZoom() {
-        meta.setAttribute("content", enabledZoom);
+        meta.setAttribute( "content", enabledZoom );
         enabled = true;
     }
     function disableZoom() {
-        meta.setAttribute("content", disabledZoom);
+        meta.setAttribute( "content", disabledZoom );
         enabled = false;
     }
-    function checkTilt(e) {
+    function checkTilt( e ) {
         aig = e.accelerationIncludingGravity;
-        x = Math.abs(aig.x);
-        y = Math.abs(aig.y);
-        z = Math.abs(aig.z);
+        x = Math.abs( aig.x );
+        y = Math.abs( aig.y );
+        z = Math.abs( aig.z );
         // If portrait orientation and in one of the danger zones
-        if (!w.orientation && (x > 7 || ((z > 6 && y < 8 || z < 8 && y > 6) && x > 5))) {
-            if (enabled) {
+        if ( !w.orientation && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ) {
+            if ( enabled ) {
                 disableZoom();
             }
         }
-        else if (!enabled) {
+        else if ( !enabled ) {
             restoreZoom();
         }
     }
-    w.addEventListener("orientationchange", restoreZoom, false);
-    w.addEventListener("devicemotion", checkTilt, false);
-})(this);
+    w.addEventListener( "orientationchange", restoreZoom, false );
+    w.addEventListener( "devicemotion", checkTilt, false );
+} )( this );
--- content/themes/digital-portfolio-lamp/single-project.php
+++ content/themes/digital-portfolio-lamp/single-project.php
@@ -1,66 +1,60 @@
 <?php get_header(); ?>
-<div id="bg-overlay">
-</div>
+
 <div id="content">
 
-	<div id="inner-content" class="wrap clearfix">
-
-		<div id="main" class="twelvecol first clearfix" role="main">
-
-			<?php
-			if ( have_posts() ) {
-				while ( have_posts() ) {
-					the_post();
-					?>
-
-					<div id="highlighted_project_image" class="sixcol first">
-						<div id="image_container">
-							<?php
-							if ( has_post_thumbnail() ) {
-								$image_url = wp_get_attachment_url( get_post_thumbnail_id() );
-							} else {
-								$image_url = "";
-							}
-							?>
-							<img src="<?php echo $image_url; ?>" />
-						</div>
+	<?php
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
+			?>
+			<div id='highlighted_project'>
+				<div id="highlighted_project_image">
+					<div id="image_container">
+						<?php
+						if ( has_post_thumbnail() ) {
+							$image_url = wp_get_attachment_url( get_post_thumbnail_id() );
+						} else {
+							$image_url = "";
+						}
+						?>
+						<img src="<?php echo $image_url; ?>" />
 					</div>
+				</div>
 
-					<div id="highlighted_project_info" class="sixcol last">
-						<h1><?php the_title(); ?></h1>
-						<p>
-							<?php the_content(); ?>
-						</p>
-						
-						<div id='related-projects-gallery'>
-							<div class='gallery-nav'>
-								<a class='nav-prev'>
-									<
-								</a>
-								<a class='nav-next'>
-									>
-								</a>
-								
-								<div class='gallery-content'>
-									<div class='img-container'>
-										
-									</div>
+				<div id="highlighted_project_info">
+					<h1><?php the_title(); ?></h1>
+					<p>
+						<?php the_content(); ?>
+					</p>
+
+					<div id='related-projects-gallery'>
+						<div class='gallery-nav'>
+							<a class='nav-prev'>
+								<
+							</a>
+							<a class='nav-next'>
+								>
+							</a>
+
+							<div class='gallery-content'>
+								<div class='img-container'>
+
 								</div>
 							</div>
 						</div>
 					</div>
+				</div>
+				
+				<div id="bg-overlay">
+				</div>
+			</div>
+		<?php } // End while loop  ?>
 
-				<?php } // End while loop  ?>
+	<?php } else { ?>
 
-			<?php } else { ?>
+		No project was found.
 
-				No project was found.
-
-			<?php } // End if  ?>
-
-		</div> <!-- end #main -->
-
-	</div> <!-- end #inner-content -->
+	<?php } // End if  ?>
 
 </div> <!-- end #content -->
 
